cb20c6f0b9f27eba40a9d423e63f5c8a8e8393c6
ArchedArtem <archedartem@gmail.com>
2026-03-24 23:19:03 +0300

    Edit components

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next";
 import "../globals.css";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Header from "@/app/components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
 
 export const metadata: Metadata = {
     title: "Обучение Руна С",
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header className="flex justify-between items-center p-4 bg-white border-b shadow-sm">
             <div className="font-bold text-xl text-blue-900">Руна С</div>
             <nav className="space-x-6">
-                <a href="/" className="hover:text-blue-600">Главная</a>
+                <a href="/public" className="hover:text-blue-600">Главная</a>
                 <a href="/catalog" className="hover:text-blue-600">Курсы 1С</a>
                 <a href="/login" className="px-4 py-2 bg-blue-600 text-white rounded-md">Вход</a>
             </nav>
