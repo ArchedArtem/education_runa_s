b8b1b84dea997bc5e1c22b4e44c0bbd3971ef4e9
ArchedArtem <archedartem@gmail.com>
2026-04-26 17:54:06 +0300

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
     return (
         <html lang="ru">
         <head>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         </head>
         <body className="flex flex-col min-h-screen w-full">
         {children}
