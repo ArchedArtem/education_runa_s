3ab6ad3dd8b0d578b06794a57464d66ecc252efb
ArchedArtem <archedartem@gmail.com>
2026-04-26 17:36:56 +0300

    fix vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
     return (
         <html lang="ru">
         <head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-            <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         </head>
         <body className="flex flex-col min-h-screen w-full">
         {children}
