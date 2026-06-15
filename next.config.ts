c88fbf3bdc58ff86829a6cad32d530f95e5b31ff
ArchedArtem <archedartem@gmail.com>
2026-06-15 11:12:50 +0300

    fix generate certificates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
+    serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
